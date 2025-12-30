40297494febe827cd82a349a9c59892b2847b116
serdar-sahin <serdar.umut@gmail.com>
2025-12-31 00:13:33 +0300

    add custom player

---
+++ Vizyo.Video.Player/Views/CustomPlayer1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace Vizyo.Video.Player.Views
+{
+    public class CustomPlayer1(int width, int height)
+    {
+        public Grid GetPlayer(string source)
+        {
+            var player = new MpvPlayerView() { Width = width, Height = height , Source = source };
+
+            var topBar = new Border
+            {
+                Background = Brush.Parse("#ffffff"),
+                Height = 30,
+                VerticalAlignment = Avalonia.Layout.VerticalAlignment.Top,
+                Opacity = 0.7,
+                Child = new TextBlock
+                {
+                    Text = "Video Player",
+                    Foreground = Brushes.Black,
+                    HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center,
+                    VerticalAlignment = Avalonia.Layout.VerticalAlignment.Center,
+                    FontSize = 14
+                }
+            };
+
+            var footerBar = new Border
+            {
+                Background = Brush.Parse("#000000"),
+                Height = 30,
+                VerticalAlignment = Avalonia.Layout.VerticalAlignment.Bottom,
+                Opacity = 0.7,
+                Child = new TextBlock
+                {
+                    Text = "Video Player",
+                    Foreground = Brushes.White,
+                    HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center,
+                    VerticalAlignment = Avalonia.Layout.VerticalAlignment.Center,
+                    FontSize = 14
+                }
+            };
+
+            var grid = new Grid()
+            {
+                Width = width,
+                Height = height,
+            };
+
+            grid.Children.Add(player);
+            grid.Children.Add(topBar);
+            grid.Children.Add(footerBar);
+
+            return grid;
+        }
+    }
+}
--- Vizyo.Video.Player/Views/MainView.axaml.cs
+++ Vizyo.Video.Player/Views/MainView.axaml.cs
@@ -17,21 +17,26 @@ public partial class MainView : UserControl
         //Canvas.SetLeft(player3, 100); Canvas.SetTop(player3, 300);
         //MainCanvas.Children.Add(player3);
 
-        var player1 = new MpvPlayerView() { Width = 300, Height = 200, Background = Brush.Parse("#000000"), Source = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" };
-        Canvas.SetLeft(player1, 10); Canvas.SetTop(player1, 10);
-        MainCanvas.Children.Add(player1);
+        //var player1 = new MpvPlayerView() { Width = 300, Height = 200, Background = Brush.Parse("#000000"), Source = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" };
+        //Canvas.SetLeft(player1, 10); Canvas.SetTop(player1, 10);
+        //MainCanvas.Children.Add(player1);
 
-        var player2 = new MpvPlayerView() { Width = 300, Height = 200, Source = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4" };
-        Canvas.SetLeft(player2, 400); Canvas.SetTop(player2, 10);
-        MainCanvas.Children.Add(player2);
+        //var player2 = new MpvPlayerView() { Width = 300, Height = 200, Source = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4" };
+        //Canvas.SetLeft(player2, 400); Canvas.SetTop(player2, 10);
+        //MainCanvas.Children.Add(player2);
 
-        var player3 = new MpvPlayerView() { Width = 300, Height = 200, Source = "https://test-videos.co.uk/vids/bigbuckbunny/mp4/av1/360/Big_Buck_Bunny_360_10s_1MB.mp4" };
-        Canvas.SetLeft(player3, 100); Canvas.SetTop(player3, 300);
-        MainCanvas.Children.Add(player3);
+        //var player3 = new MpvPlayerView() { Width = 300, Height = 200, Source = "https://test-videos.co.uk/vids/bigbuckbunny/mp4/av1/360/Big_Buck_Bunny_360_10s_1MB.mp4" };
+        //Canvas.SetLeft(player3, 100); Canvas.SetTop(player3, 300);
+        //MainCanvas.Children.Add(player3);
 
         //MainCanvas.Children.Add(new PlayerView() { Width = 300, Height = 200, Source = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" });
         //MainCanvas.Children.Add(new PlayerView() { Width = 300, Height = 200, Source = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4" });
         //MainCanvas.Children.Add(new PlayerView() { Width = 300, Height = 200, Background=Brush.Parse("#000000"), Source = "https://test-videos.co.uk/vids/bigbuckbunny/mp4/av1/360/Big_Buck_Bunny_360_10s_1MB.mp4" });
         //MainCanvas.Children.Add(new PlayerView() { Width = 300, Height = 200, Source = "stock-video.mp4" });
+
+
+        var grid = new CustomPlayer1(600, 340).GetPlayer("https://test-videos.co.uk/vids/bigbuckbunny/mp4/av1/360/Big_Buck_Bunny_360_10s_1MB.mp4");
+        Canvas.SetLeft(grid, 10); Canvas.SetTop(grid, 10);
+        MainCanvas.Children.Add(grid);
     }
 }
